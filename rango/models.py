e616179916b2ea4c307cf6ecdd7748a6f3c517fa
Alessia Masola <2202343m@student.gla.ac.uk>
2017-02-06 21:33:05 +0000

    Chapter 9.9 . Not sure if I should add the new code to base.html or index.html . Confusedgit add .! Sidebar not working properlygit add . Help!

--- rango/models.py
+++ rango/models.py
@@ -10,7 +10,6 @@ class Category(models.Model):
 	name = models.CharField(max_length=128, unique=True)
 	views = models.IntegerField(default=0)
 	likes = models.IntegerField(default=0)
-	slug = models.SlugField(blank=True)
 	slug = models.SlugField(unique=True)
 	
 	def save(self, *args, **kwargs):
